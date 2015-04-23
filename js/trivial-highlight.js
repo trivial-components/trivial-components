2a816ac9861ef76b28d06e012864e43836c11dc6
Yann Massard <yamass@gmail.com>
2015-04-23 19:40:16 +0200

    Introduced API object

---
+++ js/trivial-highlight.js
@@ -0,0 +1,29 @@
+(function ($) {
+    $.expr[":"].containsIgnoreCase = $.expr.createPseudo(function (arg) {
+        return function (elem) {
+            return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+        };
+    });
+})(jQuery);
+
+(function ($) {
+    $.fn.highlight = function (searchString, highlightClassName) {
+        var regex = new RegExp(searchString, "gi");
+        return this.find('*').each(function () {
+            var $this = $(this);
+
+            $this.find('.' + highlightClassName).contents().unwrap();
+            this.normalize();
+
+            if (searchString && searchString !== '') {
+                $this.contents().filter(function () {
+                    return this.nodeType == 3 && regex.test(this.nodeValue);
+                }).replaceWith(function () {
+                    return (this.nodeValue || "").replace(regex, function (match) {
+                        return "<span class=\"" + highlightClassName + "\">" + match + "</span>";
+                    });
+                });
+            }
+        });
+    };
+}(jQuery));
