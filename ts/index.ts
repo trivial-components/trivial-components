fa24844ae3ea07330ff58d123dfe80c9437411c5
Yann Massard <yamass@gmail.com>
2018-02-27 10:04:27 +0100

    fixed package json for use as npm package

---
+++ ts/index.ts
@@ -0,0 +1,32 @@
+/*
+ *
+ *  Copyright 2016 Yann Massard (https://github.com/yamass) and other contributors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+export * from "./trivial-highlight";
+export * from "./TrivialCalendarBox";
+export * from "./TrivialComboBox";
+export * from "./TrivialCore";
+export * from "./TrivialDateSuggestionEngine";
+export * from "./TrivialDateTimeField";
+export * from "./TrivialEvent";
+export * from "./TrivialListBox";
+export * from "./TrivialTagComboBox";
+export * from "./TrivialTimeSuggestionEngine";
+export * from "./TrivialTree";
+export * from "./TrivialTreeBox";
+export * from "./TrivialTreeComboBox";
+export * from "./TrivialUnitBox";
