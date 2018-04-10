7d9200f7ce87690c687708664b1562095380660e
Yann Massard <yamass@gmail.com>
2018-04-10 15:36:47 +0200

    reworked pages: es6 modules, es6 module support in monaco editor

--- ts/demo.ts
+++ ts/demo.ts
@@ -7,7 +7,7 @@ import '../less/all.less';
 import * as jQuery from "jquery";
 (window as any).$ = jQuery;
 
-if (location.pathname === '/demo.html') {
+if (location.pathname.indexOf('/demo.html') !== -1) {
 	jQuery.get("ts/examples/demo.json", function(data) {
 		(window as any).exampleEditor = new ExampleEditor('#exampleEditor', data);
 	});
